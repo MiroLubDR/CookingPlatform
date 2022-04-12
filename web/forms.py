beea7970cc5091b7227e6417a7c017e4931f6f4e
MiroLubDR <103529152+MiroLubDR@users.noreply.github.com>
2022-04-12 21:13:01 +0300

    Add files via upload

---
+++ web/forms.py
@@ -0,0 +1,21 @@
+from django import forms as djangoForms
+
+from djangoProject1.web.models import Recipe
+
+
+class RecipeForm(djangoForms.ModelForm):
+    def __init__(self,user,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.user = user
+    def save(self, commit=True):
+        u =super().save(commit=False)
+        u.user = self.user
+        if commit :
+            u.save()
+        return u
+        pass
+
+    class Meta:
+        model = Recipe
+        fields = ['recipe_img','recipe_name','products','description']
+
